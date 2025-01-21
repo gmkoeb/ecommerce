c3c474c32ca8fede4c3406783b1ad0e3a49e5cd3
Gabriel Manika <gmkoeb@gmail.com>
2025-01-21 11:38:44 -0300

    Add linting/formatting tools

--- api/ecommerce/database/database.py
+++ api/ecommerce/database/database.py
@@ -8,9 +8,11 @@ load_dotenv()
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost") 
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 
-database_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
+database_url = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
+)
 engine = create_engine(database_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- api/ecommerce/user/__init__.py
+++ api/ecommerce/user/__init__.py
@@ -3,10 +3,10 @@
 from sqlalchemy import Column, Integer, String
 from ecommerce.database.base import Base
 
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     email = Column(String, unique=True)
     password = Column(String)
-    
