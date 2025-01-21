9c917fbd598a00745c4f1b43f97b51b2c9a46258
Gabriel Manika <gmkoeb@gmail.com>
2025-01-21 16:17:17 -0300

    fix: reformat black warnings

--- api/ecommerce/user/repository/users_repository.py
+++ api/ecommerce/user/repository/users_repository.py
@@ -2,11 +2,14 @@
 
 from ecommerce.user import User
 
+
 class UsersRepository:
     def __init__(self, db):
         self.__db = db
 
-    def create_user(self, name: str = "", email: str = "", password: str = "") -> User:
+    def create_user(
+        self, name: str = "", email: str = "", password: str = ""
+    ) -> User | None:
         """Inserts a user into the database"""
         new_user = User(name=name, email=email, password=password)
 
