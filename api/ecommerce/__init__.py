8d91fdf5010fa7ff20f30cbedc1f5dc8f565e2fd
Gabriel Manika <gmkoeb@gmail.com>
2025-01-23 16:39:47 -0300

    fix:change database session setup
    - Now the database session is set during the flask app initialization,
    as a config variable.
    
    - This removes the need to dynamically change the database based on
    the environment variable, and allows a better control of the testing
    db in fixtures.

--- api/ecommerce/__init__.py
+++ api/ecommerce/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from ecommerce.user.web import bp as users_bp
+from ecommerce.database.database import SessionLocal
 
 app = Flask(__name__)
+app.config["DB_SESSION"] = SessionLocal()
 app.register_blueprint(users_bp, url_prefix="/api/v1")
--- api/ecommerce/database/database.py
+++ api/ecommerce/database/database.py
@@ -5,18 +5,10 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-if os.environ.get("FLASK_ENV") == "test":
-    from ecommerce.database.base import Base
-
-    engine = create_engine("sqlite:///:memory:")
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    Base.metadata.create_all(engine)
-else:
-    load_dotenv()
-    POSTGRES_USER = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_DB = os.getenv("POSTGRES_DB")
-    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
-    database_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
-    engine = create_engine(database_url)
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
+database_url = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
+engine = create_engine(database_url)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
