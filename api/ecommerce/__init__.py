7987f067f27b60ee2e7d6bd2e3111f4476d6ab03
Gabriel Manika <gmkoeb@gmail.com>
2025-01-24 14:59:40 -0300

    feat: install and configure flask-cors

--- api/ecommerce/__init__.py
+++ api/ecommerce/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from ecommerce.user.web import bp as users_bp
 from ecommerce.database.database import SessionLocal
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["DB_SESSION"] = SessionLocal()
 app.register_blueprint(users_bp, url_prefix="/api/v1")
