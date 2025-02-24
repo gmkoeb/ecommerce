c713c471c959937bb2d48879abb6740c7d4bebf7
Gabriel Manika <gmkoeb@gmail.com>
2025-02-24 10:49:50 -0300

    refactor: change global fonts
    - Now using font Kanit for heading elements and Montserrat for global
    texts

--- web/app/sign_up/page.tsx
+++ web/app/sign_up/page.tsx
@@ -1,14 +1,21 @@
 'use client'
-import { ErrorMessage, Field, Form, Formik, FormikErrors, FormikHelpers } from "formik";
-import { api } from "../../lib/axios";
-import { AxiosResponse } from "axios";
-import { useState } from "react";
+import type { AxiosResponse } from 'axios'
+import {
+  ErrorMessage,
+  Field,
+  Form,
+  Formik,
+  type FormikErrors,
+  type FormikHelpers,
+} from 'formik'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { api } from '../../lib/axios'
 
 interface SignUpFormValues {
-  name: string,
-  email: string,
-  password: string,
+  name: string
+  email: string
+  password: string
   confirm_password: string
 }
 
@@ -19,18 +26,20 @@ export default function SignUp() {
     email: '',
     name: '',
     password: '',
-    confirm_password: ''
+    confirm_password: '',
   }
 
-  async function handleSubmit(values: SignUpFormValues, actions: FormikHelpers<SignUpFormValues>){
+  async function handleSubmit(
+    values: SignUpFormValues,
+    actions: FormikHelpers<SignUpFormValues>
+  ) {
     try {
-      const userData = { 
+      const userData = {
         user: {
           email: values.email,
           password: values.password,
-          name: values.name
-        }
-
+          name: values.name,
+        },
       }
       await api.post<AxiosResponse>('/sign_up', userData)
       actions.setSubmitting(false)
@@ -41,30 +50,35 @@ export default function SignUp() {
     }
   }
 
-  return(
+  return (
     <div className="grid grid-cols-2 w-full">
-      <img src="signupcover.jpg" alt="Picture of electronic products" className="h-[85vh] mt-2 rounded-lg ml-10"/>
+      <img
+        src="signupcover.jpg"
+        alt="Electronic products"
+        className="h-[85vh] mt-2 rounded-lg ml-10"
+      />
       <Formik
         initialValues={initialValues}
         onSubmit={(values, actions) => handleSubmit(values, actions)}
         validate={(values: SignUpFormValues) => {
           const errors: FormikErrors<SignUpFormValues> = {}
-          if (!values.name){
-            errors.name = "Required"
+          if (!values.name) {
+            errors.name = 'Required'
           }
 
-          if (!values.email){
-            errors.email = "Required"
+          if (!values.email) {
+            errors.email = 'Required'
           }
 
-          if (!values.password){
-            errors.password = "Required"
+          if (!values.password) {
+            errors.password = 'Required'
           }
 
-          if (!values.confirm_password){
-            errors.confirm_password = "Required"
-          } else if (values.confirm_password != values.password){
-            errors.confirm_password = "Password confirmation and password must be equal."
+          if (!values.confirm_password) {
+            errors.confirm_password = 'Required'
+          } else if (values.confirm_password != values.password) {
+            errors.confirm_password =
+              'Password confirmation and password must be equal.'
           }
 
           return errors
@@ -72,85 +86,112 @@ export default function SignUp() {
       >
         {({ errors, touched }) => (
           <Form className="flex flex-col mt-20 justify-center gap-6 text-left ml-40">
-            {apiErrors.length > 0 &&
-                <>
-                  {apiErrors.map(error => (
-                    <div className="flex flex-col gap-2 mt-10 border border-red-500 w-1/2 items-center rounded" key={error}>
-                      <h3 className="text-lg font-bold text-center">There was an error while creating your account:</h3>
-                      <p className="text-red-500 font-bold">*{error}</p>
-                    </div>
-                  ))}
-                </>
-              }
+            {apiErrors.length > 0 && (
+              <div>
+                {apiErrors.map(error => (
+                  <div
+                    className="flex flex-col gap-2 mt-10 border border-red-500 w-1/2 items-center rounded"
+                    key={error}
+                  >
+                    <h3 className="text-lg font-bold text-center">
+                      There was an error while creating your account:
+                    </h3>
+                    <p className="text-red-500 font-bold">*{error}</p>
+                  </div>
+                ))}
+              </div>
+            )}
             <h1 className="text-5xl text-left font-bold">Sign up</h1>
             <div className="w-1/2 flex flex-col">
               <div className="flex justify-between">
                 <label htmlFor="name">Name</label>
-                <ErrorMessage className="text-red-500 font-bold" name="name" component={'div'}></ErrorMessage>
+                <ErrorMessage
+                  className="text-red-500 font-bold"
+                  name="name"
+                  component={'div'}
+                />
               </div>
-              <Field 
+              <Field
                 className={
-                  errors.name && touched.name ? 
-                  "bg-neutral-100 border border-red-500 rounded-lg p-2" : 
-                  "bg-neutral-100 border border-white rounded-lg p-2"
-                }  
-                id="name" 
-                name="name" 
-                type="text" 
-                placeholder="Type your name...">
-              </Field>
+                  errors.name && touched.name
+                    ? 'bg-neutral-100 border border-red-500 rounded-lg p-2'
+                    : 'bg-neutral-100 border border-white rounded-lg p-2'
+                }
+                id="name"
+                name="name"
+                type="text"
+                placeholder="Type your name..."
+              />
             </div>
             <div className="w-1/2 flex flex-col">
               <div className="flex justify-between">
                 <label htmlFor="email">Email</label>
-                <ErrorMessage className="text-red-500 font-bold" name="email" component={'div'}></ErrorMessage>
+                <ErrorMessage
+                  className="text-red-500 font-bold"
+                  name="email"
+                  component={'div'}
+                />
               </div>
-              <Field 
+              <Field
                 className={
-                  errors.email && touched.email ? 
-                  "bg-neutral-100 border border-red-500 rounded-lg p-2" : 
-                  "bg-neutral-100 rounded-lg p-2"
-                }  
-                id="email" 
-                name="email" 
-                type="text" 
-                placeholder="Type your email...">
-
-              </Field>
+                  errors.email && touched.email
+                    ? 'bg-neutral-100 border border-red-500 rounded-lg p-2'
+                    : 'bg-neutral-100 rounded-lg p-2'
+                }
+                id="email"
+                name="email"
+                type="text"
+                placeholder="Type your email..."
+              />
             </div>
             <div className="w-1/2 flex flex-col">
               <div className="flex justify-between">
                 <label htmlFor="password">Password</label>
-                <ErrorMessage className="text-red-500 font-bold" name="password" component={'div'}></ErrorMessage>
+                <ErrorMessage
+                  className="text-red-500 font-bold"
+                  name="password"
+                  component={'div'}
+                />
               </div>
-              <Field className={
-                  errors.password && touched.password ? 
-                  "bg-neutral-100 border border-red-500 rounded-lg p-2" : 
-                  "bg-neutral-100 rounded-lg p-2"
-                }  
-                id="password" 
-                name="password" 
-                type="password" 
-                placeholder="Type your password...">
-              </Field>
+              <Field
+                className={
+                  errors.password && touched.password
+                    ? 'bg-neutral-100 border border-red-500 rounded-lg p-2'
+                    : 'bg-neutral-100 rounded-lg p-2'
+                }
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Type your password..."
+              />
             </div>
             <div className="w-1/2 flex flex-col">
               <div className="flex justify-between">
                 <label htmlFor="confirm_password">Confirm Password</label>
-                <ErrorMessage className="text-red-500 font-bold text-center" name="confirm_password" component={'div'}></ErrorMessage>
+                <ErrorMessage
+                  className="text-red-500 font-bold text-center"
+                  name="confirm_password"
+                  component={'div'}
+                />
               </div>
-              <Field 
+              <Field
                 className={
-                  errors.confirm_password && touched.confirm_password ? 
-                  "bg-neutral-100 border border-red-500 rounded-lg p-2" : 
-                  "bg-neutral-100 rounded-lg p-2"
-                } 
-                id="confirm_password" name="confirm_password" 
-                type="password" 
-                placeholder="Confirm Password">
-              </Field>
+                  errors.confirm_password && touched.confirm_password
+                    ? 'bg-neutral-100 border border-red-500 rounded-lg p-2'
+                    : 'bg-neutral-100 rounded-lg p-2'
+                }
+                id="confirm_password"
+                name="confirm_password"
+                type="password"
+                placeholder="Confirm Password"
+              />
             </div>
-            <button className="w-1/2 bg-blue-500 text-white py-2 rounded-full text-lg" type="submit">Sign Up</button>
+            <button
+              className="w-1/2 bg-blue-500 text-white py-2 rounded-full text-lg"
+              type="submit"
+            >
+              Sign Up
+            </button>
           </Form>
         )}
       </Formik>
--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -1,19 +1,19 @@
 'use client'
-import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useCart } from '@/context/CartContext'
 import Cookies from 'js-cookie'
-import Cart from "./Cart";
-import { ShoppingBasket } from "lucide-react";
-import { useCart } from "@/context/CartContext";
-import { useRouter } from "next/navigation";
+import { ShoppingBasket } from 'lucide-react'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import Cart from './Cart'
 
-export default function Header(){
+export default function Header() {
   const router = useRouter()
   const [userName, setUsername] = useState('')
   const { cartCounter } = useCart()
   const [cartClicked, setCartClicked] = useState<boolean>(false)
-  
-  async function handleLogout(){
+
+  async function handleLogout() {
     Cookies.remove('token')
     Cookies.remove('userName')
     router.refresh()
@@ -22,9 +22,9 @@ export default function Header(){
 
   useEffect(() => {
     const name = Cookies.get('userName')
-    if (name){
+    if (name) {
       const splittedName = name.split(' ')
-      if (splittedName.length >= 3){
+      if (splittedName.length >= 3) {
         setUsername(splittedName.splice(0, 2).join(' '))
       } else {
         setUsername(name)
@@ -32,32 +32,62 @@ export default function Header(){
     }
   }, [])
 
-  return(
+  return (
     <header className="pb-10 mb-14">
       <nav className="flex justify-between py-4 bg-violet-600 text-neutral-200 fixed w-full z-20 items-center">
-        <Link href={'/'} className="font-bold text-3xl mx-10">Ecommerce<span className="text-lg">.com.br</span></Link>
-        <input type="text" className="w-3/4 rounded-lg py-2 px-4" placeholder="Search Ecommerce.com.br" />
+        <Link
+          href={'/'}
+          className="font-heading font-semibold text-4xl mx-10 tracking-wide"
+        >
+          Ecommerce<span className="text-lg">.com.br</span>
+        </Link>
+        <input
+          type="text"
+          className="w-3/4 rounded-lg py-2 px-4"
+          placeholder="Search Ecommerce.com.br"
+        />
         <div className="flex gap-4 mr-4 items-center">
-          {userName ? 
+          {userName ? (
             <div className="flex flex-col ml-5">
               <h5 className="text-center font-bold w-32">{userName}</h5>
-              <button className="hover:underline" onClick={() => handleLogout()}>Logout</button>
+              <button
+                type="button"
+                className="hover:underline"
+                onClick={() => handleLogout()}
+              >
+                Logout
+              </button>
             </div>
-          :
+          ) : (
             <>
-              <Link className="p-2 rounded-lg text-md hover:opacity-80 duration-300 text-center font-bold" href={'/sign_in'}>Sign in to your account</Link>
+              <Link
+                className="p-2 rounded-lg text-md hover:opacity-80 duration-300 text-center font-bold"
+                href={'/sign_in'}
+              >
+                Sign in to your account
+              </Link>
             </>
-          }
+          )}
           <div className="relative mt-4 text-sm">
-            <div className="fixed right-10 bg-red-500 rounded-full px-2 font-bold hover:cursor-pointer" onClick={() => cartClicked ? setCartClicked(false) : setCartClicked(true)}>
+            <div
+              className="fixed right-10 bg-red-500 rounded-full px-2 font-bold hover:cursor-pointer"
+              onClick={() =>
+                cartClicked ? setCartClicked(false) : setCartClicked(true)
+              }
+            >
               {cartCounter}
             </div>
           </div>
           <div className="flex flex-col items-center">
-            <ShoppingBasket onClick={() => cartClicked ? setCartClicked(false) : setCartClicked(true)} className="hover:cursor-pointer" size={50} strokeWidth={1} />
-            {cartClicked &&
-              <Cart />
-            }
+            <ShoppingBasket
+              onClick={() =>
+                cartClicked ? setCartClicked(false) : setCartClicked(true)
+              }
+              className="hover:cursor-pointer"
+              size={50}
+              strokeWidth={1}
+            />
+            {cartClicked && <Cart />}
           </div>
         </div>
       </nav>
